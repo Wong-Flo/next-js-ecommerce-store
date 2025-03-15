7c42cb734f37c77b6db727e6e26859e8f4b90ea6
Wong-Flo <florian.wong@outlook.com>
2025-03-15 23:25:26 +0100

    Add something to for the main page

--- app/page.js
+++ app/page.js
@@ -21,21 +21,30 @@ export default function Home() {
           color: '#444',
         }}
       >
-        <p>
-          Welcome to our coffee shop! We believe that coffee is more than just a
-          drink; it's an experience. Our mission is to provide you with the best
-          coffee experience possible, from the moment you walk through our doors
-          to the last sip of your favorite brew.
-        </p>
-        <p>
-          Whether you're a coffee aficionado or just looking for a cozy place to
-          relax, we have something for everyone. Our menu features a wide
-          variety of coffee drinks, teas, and pastries, all made with the finest
-          ingredients.
-        </p>
-        <p>
-          Join us for a cup of coffee and discover why we say, "Just Brew It!"
-        </p>
+        <div
+          style={{
+            padding: '20px',
+            backgroundColor: '#f9f9f9',
+            borderRadius: '5px',
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+          }}
+        >
+          <p>
+            Welcome to our coffee shop! We believe that coffee is more than just
+            a drink; it's an experience. Our mission is to provide you with the
+            best coffee experience possible, from the moment you walk through
+            our doors to the last sip of your favorite brew.
+          </p>
+          <p>
+            Whether you're a coffee aficionado or just looking for a cozy place
+            to relax, we have something for everyone. Our menu features a wide
+            variety of coffee drinks, teas, and pastries, all made with the
+            finest ingredients.
+          </p>
+          <p>
+            Join us for a cup of coffee and discover why we say, "Just Brew It!"
+          </p>
+        </div>
       </div>
     </div>
   );
